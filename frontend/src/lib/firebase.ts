2c7f3c452aabfae6d145c7334cc45c2447f943a2
AbhayKTS <abhay88998@gmail.com>
2026-07-12 11:05:03 +0530

    securing API keys

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZzH23Z-SiSl0Wc6rMdkblT9U3GibXZ2M",
-  authDomain: "odoo-hack-bc2bd.firebaseapp.com",
-  projectId: "odoo-hack-bc2bd",
-  storageBucket: "odoo-hack-bc2bd.firebasestorage.app",
-  messagingSenderId: "756277398800",
-  appId: "1:756277398800:web:c08318ee4137c29a97e995",
-  measurementId: "G-CDB3JY8TE1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
